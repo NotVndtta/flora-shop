3810268e8ee90280f863bbb57ca1380faefee9be
NotVndtta <thenotalexey192@gmail.com>
2024-04-08 01:58:35 +0300

    Navbar markup

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div >
+    <ul className='flex items-center justify-between p-4'>
+      <a href="">Flora Shop</a>
+      <a href="">Phone number</a>
+      <a href="">Adress</a>
+      <a href="">Mail</a>
+    </ul>
+    <hr />
+    </div>
+  )
+}
+
+export default Navbar
