9ce1bab60a7da9e651206f71184f305728699944
NotVndtta <thenotalexey192@gmail.com>
2024-04-20 23:34:47 +0300

    About page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-
+import water from '../img/water.png'
+import flower from '../img/flower.png'
+import time from '../img/time.png'
 const About = () => {
   return (
-    <div className='flex flex-col justify-center items-center text-white bg-gray-700 text-3xl leading-normal py-20 px-5'>
+    <div className='flex flex-col justify-center items-center text-white bg-gray-700 text-2xl leading-normal py-20 px-5'>
         <h1>Почему нужно выбрать именно нас:</h1>
       <div className="flex justify-between items-center gap-10 p-5">
         <div className="">
+            <img src={water} alt="water" className='mx-auto'/>
             <h2>Только свежие цветы из Европы</h2>
             <h3>Привоз 4 раза в неделю</h3>
         </div>
         <div className="">
+            <img src={flower} alt="flower" className='mx-auto'/>
             <h2>Оформление букетов флористами</h2>
             <h3>Букеты собираются опытными флористами</h3>
         </div>
         <div className="">
+            <img src={time} alt="time" className='mx-auto'/>
             <h2>Цветы стоят более недели и не теряют свой вид</h2>
             <h3>Мы умеем заботиться о цветах и клиентах</h3>
         </div>
